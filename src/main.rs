0eb6d7ec4dc8ca68ad640ab5d361e8b229c9aaa8
Kursat Kobya <kursat.kobya@gmail.com>
2021-08-19 13:30:51 +0200

    Get the headers of HTTP/GET Request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
 
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+  let res = reqwest::get("http://httpbin.org/get").await?;
+  println!("Status: {}", res.status());
+  println!("Headers:\n{:#?}", res.headers());
+
+  let body = res.text().await?;
+  println!("Body:\n{}", body);
+  Ok(())
 }
