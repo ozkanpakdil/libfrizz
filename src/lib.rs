4f372c4e194421af47c18e0aa3a8de772ad38f30
Kursat Kobya <kursat.kobya@gmail.com>
2021-08-21 12:53:24 +0200

    Add a library and move get_header method

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub async fn get_header() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("http://httpbin.org/get").await?;
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+
+    let body = res.text().await?;
+    println!("Body:\n{}", body);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-  let res = reqwest::get("http://httpbin.org/get").await?;
-  println!("Status: {}", res.status());
-  println!("Headers:\n{:#?}", res.headers());
-
-  let body = res.text().await?;
-  println!("Body:\n{}", body);
-  Ok(())
+    libfrizz::get_header();
+    Ok(())
 }
