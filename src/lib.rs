e94234a41c721c061f074d57b85e71b25b443c74
Ozkan Pakdil <ozkan.pakdil@gmail.com>
2021-08-26 11:58:10 +0100

    #4 - dprint used for formatting - ansi_term for coloring the command line - clap for command line args

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,39 @@
-pub async fn get_header() -> Result<(), reqwest::Error> {
-    let res = reqwest::get("http://httpbin.org/get").await?;
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
+pub struct FizzResult {
+    pub status_code: String,
+    pub headers: String,
+    pub body: String,
+}
 
-    let body = res.text().await?;
-    println!("Body:\n{}", body);
-    Ok(())
+pub async fn get_header(url: &str) -> Result<FizzResult, reqwest::Error> {
+    let res = reqwest::get(url).await?;
+    Ok(
+        FizzResult {
+            status_code: res.status().to_string(),
+            headers: format!("Headers:\n{:#?}", res.headers()),
+            body: res.text().await?,
+        }
+    )
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod tests {
+    use super::*;
+    use ansi_term::Colour;
+
+    #[tokio::test]
+    async fn test_get_header() {
+        let res = get_header("http://httpbin.org/get").await.unwrap();
+        println!("{}", Colour::Red.paint(res.status_code));
+        println!("{}", Colour::Green.paint(res.headers));
+        println!("{}", Colour::Blue.paint(res.body));
+    }
+    #[tokio::test]
+    #[should_panic]
+    async fn test_get_header_error() {
+        let res = get_header("httpjhb://httpbin.org/get").await.unwrap();
+        println!("{}", Colour::Red.paint(res.status_code));
+        println!("{}", Colour::Green.paint(res.headers));
+        println!("{}", Colour::Blue.paint(res.body));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,47 @@
+use std::convert::TryFrom;
+use std::env;
+use std::io::{Error, ErrorKind};
+
+use ansi_term::Colour;
+use clap::{App, load_yaml};
+use dprint_core::formatting::{PrintItems, PrintOptions};
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    libfrizz::get_header();
+async fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    let yaml = load_yaml!("cli.yaml");
+    let cmd_args = App::from(yaml).get_matches();
+
+    if args.len() == 1 {
+        App::from(yaml).print_help();
+        return Err(Error::new(ErrorKind::Other, "\n\nPlease provide the parameters!"));
+    }
+
+    if cmd_args.is_present("target") {
+        let res = libfrizz::get_header(cmd_args.value_of("target").unwrap()).await.ok().unwrap();
+        let body = res.body;
+        println!("{}", Colour::Green.paint(res.status_code));
+        if cmd_args.is_present("dump-header") {
+            println!("{}", Colour::Blue.paint(res.headers));
+        }
+
+        if cmd_args.is_present("pretty"){
+            println!("{}", Colour::White.paint(
+                dprint_core::formatting::format(|| {
+                    {
+                        PrintItems::try_from(body).unwrap()
+                    }
+                }, PrintOptions {
+                    indent_width: 4,
+                    max_width: 10,
+                    use_tabs: false,
+                    new_line_text: "\n",
+                })
+            ));
+        } else {
+            println!("{}", Colour::White.paint(body));
+        }
+    }
+
     Ok(())
 }
