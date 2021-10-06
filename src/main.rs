7039171a8804ecbec0b69b64e111c6f4452b5f2b
Kursat Kobya <kursat.kobya@gmail.com>
2021-10-06 18:02:34 +0200

    Add port scanning feature
    This patch allows user to scan ports on given adress. Currently it does
    not recognize http headers. Example usage :
    
    > frizz -s -t example.net                                                                                                      101 ↵ kursat@kursat
    ip addr 93.184.216.34
    80
    443
    1119
    1935

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{Error, Write};
-use std::{env, io};
-
+use std::{env, io,
+          net::{IpAddr, SocketAddr, ToSocketAddrs},
+          time::Duration,};
+use futures::{stream, StreamExt};
 use ansi_term::Colour;
 use clap::{load_yaml, App, ArgMatches};
 use dprint_core::formatting::PrintOptions;
@@ -25,6 +27,9 @@ async fn main() -> Result<(), Error> {
     }
 
     if cmd_args.is_present("target") {
+
+        let target = cmd_args.value_of("target").unwrap();
+
         let insecure = cmd_args.is_present("insecure");
         let verbose = cmd_args.is_present("verbose");
         if verbose {
@@ -32,7 +37,6 @@ async fn main() -> Result<(), Error> {
         } else {
             simple_logger::init_with_level(log::Level::Info).unwrap();
         }
-        let target = cmd_args.value_of("target").unwrap();
         let user_agent = if cmd_args.is_present("user-agent") {
             String::from(cmd_args.value_of("user-agent").unwrap())
         } else {
@@ -55,6 +59,17 @@ async fn main() -> Result<(), Error> {
             )
             .await
         } else {
+            if cmd_args.is_present("scan") {
+                let socket_addresses: Vec<SocketAddr> = format!("{}:0", target).to_socket_addrs()?.collect();
+                if socket_addresses.is_empty() {
+                    println!("Socket_addresses list is empty");
+                    return Ok(());
+                }
+                println!("ip addr {}", socket_addresses[0].ip());
+                scan(socket_addresses[0].ip(), 1000, 1).await;
+                return Ok(());
+            }
+
             // assume telnet or any socket protocol
             open_socket_target(target).await.ok();
         }
@@ -149,5 +164,26 @@ async fn execute_http(
         out_writer
             .write(format!("{}", Colour::White.paint(body)).as_bytes())
             .ok();
+    }
+}
+
+
+async fn scan(target: IpAddr, concurrency: usize, timeout: u64) {
+    let ports = stream::iter(1..=u16::MAX);
+
+    ports
+        .for_each_concurrent(concurrency, |port| scan_port(target, port, timeout))
+        .await;
+}
+
+async fn scan_port(target: IpAddr, port: u16, timeout: u64) {
+    let timeout = Duration::from_secs(timeout);
+    let socket_address = SocketAddr::new(target, port);
+
+    if tokio::time::timeout(timeout, TcpStream::connect(&socket_address))
+        .await
+        .is_ok()
+    {
+        println!("{}", port);
     }
 }
